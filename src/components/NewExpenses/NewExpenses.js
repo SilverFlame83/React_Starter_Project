87eb83011c2403c67fe880c4d6179cb06fc0900d
SilverFlame83 <imren_83@abv.bg>
2022-11-25 11:29:02 +0200

    Add new expense button is implemented

--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
 import "./NewExpenses.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpenses = (props) => {
+  const [editExpense, setEditExpense] = useState(false);
+
   const expenseChangeHandler = (expenseData) => {
     const newData = {
       ...expenseData,
       id: Math.random().toString(),
     };
     props.onExpenseChange(newData)
+    setEditExpense(false)
   };
+  const expenseHandler = ()=>{
+    setEditExpense(true);
+  }
+  
+  const cancelHandler=()=>{
+    setEditExpense(false);
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onExpenseChange={expenseChangeHandler} />
+      {!editExpense && <button onClick={expenseHandler}>Add Expense</button>}
+      {editExpense && <ExpenseForm onCancel={cancelHandler} onExpenseChange={expenseChangeHandler} />}
+      
     </div>
   );
 };
